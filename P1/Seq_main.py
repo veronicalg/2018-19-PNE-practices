9b51ef96c40ae1ad76c068dacd73c3193ce2535e
veronicalg <veronicalopezgonzal@hotmail.com>
2019-02-14 10:49:39 +0100

    Seq_main program in process.

--- P1/Seq_main.py
+++ P1/Seq_main.py
@@ -1,15 +1,32 @@
 from Seq import Seq
-Seq()
+
+
+# Seq()
+
+
+s1 = Seq("ACGTCA")
+s2 = Seq("GGGGGGGA")
+s3 = Seq.complement(s1)
+s4 = Seq.complement(s1).reverse(s1) #We cannot atribute reverse to a strin object.
+list = [s1, s2, s3, s4]
 def main():
-    """............"""
-    s1= Seq("ACGTCA")
-    s2 = Seq("GGGGGGGA")
-    s3 = Seq.complement(s1)
-    s4 = Seq.reverse(s3)
+    """This function organices our program and prints the 4 sequences, its length, number of bases and percentage of bases"""
 
-    list = [s1,s2,s3,s4]
+    cont = 1
+    for sequence in list:
+        print("The sequence ", cont, "is: ", sequence.get_strbases())
+        cont = cont + 1
+        print("The total length is: ", sequence.len())
+        # dic_bases = count_bases(i)
+        # print("Bases count: ", dic_bases)
+        for base in sequence:
+            dic = {}
+            dic[base] = base.count(base)
+        print("The number of bases of every type are: ", dic[base])
+        for base in sequence:
+            dict_perc = {}
+            dict_perc[base] = base.perc(base)
+        print('The percentage of bases of every type are: ', dict_perc[base])
 
-    for i in list:
-        return "Sequence", i.get_strbases(), "Length: ", i.len(),  "Bases count: ", i.
 
 
