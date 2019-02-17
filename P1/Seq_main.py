132e5667f3e618555faad547e00b806074815cc6
veronicalg <veronicalopezgonzal@hotmail.com>
2019-02-17 23:46:45 +0100

    Exercise 2 from session 7 finished.

--- P1/Seq_main.py
+++ P1/Seq_main.py
@@ -19,15 +19,15 @@ dict_perc = {}
 
 #For every object in the  list, it is going to be printed: the string sequence, length, a dictionary with the number of bases and a dictionary with the % of bases.
 for sequence in list:
-    print("The sequence ", cont, "is: ", sequence.get_strbases()) #Calling the method .get_strbases().
-    print("The length of the sequence is: ", sequence.len()) #Calling the method .len().
+    print("Sequence ", cont, ":  ", sequence.get_strbases()) #Calling the method .get_strbases().
+    print("Length : ", sequence.len()) #Calling the method .len().
 
     bases_list = ["A", "C", "T", "G"]
 
     for i in bases_list:
         dic[i] = sequence.get_strbases().count(i) #Calling the method .count(base).
         dict_perc[i] = sequence.perc(i) #Calling the method .perc()
-    print("The number of bases in the sequence", cont, "is: ", dic)
-    print("The percentage of every base in the sequence", cont, "is", dict_perc)
+    print("Bases count : ", dic)
+    print("Bases percentage :", dict_perc)
     cont = cont + 1
 
