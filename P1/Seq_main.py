d08a9d44910acb5ea9b9c5518fcbdd25a23101cc
veronicalg <veronicalopezgonzal@hotmail.com>
2019-02-17 23:34:24 +0100

    Seq_main program finished

--- P1/Seq_main.py
+++ P1/Seq_main.py
@@ -1,29 +1,27 @@
 from Seq import Seq
+# We have imported the class Seq.
 
-# Seq()
-
-
+#Creating 4 objects: s1,s2,s3,s4
 s1 = Seq("ACGTCA")
 s2 = Seq("GGGGGGGA")
 s3 = Seq(s1.complement())
-s4 = Seq(s3.reverse())  # We cannot atribute reverse to a string object.
-list = [s1, s2, s3, s4]
-# """This function organices our program and prints the 4 sequences, its length, number of bases and percentage of bases"""
+s4 = Seq(s3.reverse())
+list = [s1, s2, s3, s4] #A list is created with the 4 objects.
+
 
 cont = 1
-for sequence in list:
-    print("The sequence ", cont, "is: ", sequence.get_strbases())
+dic = {} #A dictionary is created to store the number of every base.
+dict_perc = {} #A dictionary is created to store the % of every base.
+for sequence in list: #For every object in the  list, it is going to be printed: the string sequence, length, a dictionary with the number of bases and a dictionary with the % of bases.
+    print("The sequence ", cont, "is: ", sequence.get_strbases()) #Calling the method .get_strbases().
+    print("The length of the sequence is: ", sequence.len()) #Calling the method .len().
+
+    bases_list = ["A", "C", "T", "G"]
+
+    for i in bases_list:
+        dic[i] = sequence.get_strbases().count(i) #Calling the method .count(base).
+        dict_perc[i] = sequence.perc(i) #Calling the method .perc()
+    print("The number of bases in the sequence", cont, "is: ", dic)
+    print("The percentage of every base in the sequence", cont, "is", dict_perc)
     cont = cont + 1
-    print(sequence.len())
-    # dic_bases = count_bases(i)
-    # print("Bases count: ", dic_bases)
-    for base in sequence.get_strbases():
-        dic = {}
-        dic[base] = base.count(base)
-    print("The number of bases of every type are: ", dic[base])
-    for base in sequence.get_strbases():
-        dict_perc = {}
-        dict_perc[base] = base.perc(base)
-    print('The percentage of bases of every type are: ', dict_perc[base])
-    # bases = ["A"]
-    # for i in bases:
+
