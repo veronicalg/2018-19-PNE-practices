e3b6ef46d68e44630d793b7082d1da9a11cc81ea
veronicalg <veronicalopezgonzal@hotmail.com>
2019-03-09 23:43:01 +0100

    session 13 files, exercise 2 included.

---
+++ session-13/webserver2.3.py
@@ -0,0 +1,65 @@
+import http.server
+import socketserver
+import termcolor
+
+# Define the Server's port
+PORT = 8005
+
+
+# Class with our Handler. It is a called derived from BaseHTTPRequestHandler
+# It means that our class inheritates all his methods and properties
+class TestHandler(http.server.BaseHTTPRequestHandler):
+
+    def do_GET(self):
+        """This method is called whenever the client invokes the GET method
+        in the HTTP protocol request"""
+
+        # Print the request line
+        termcolor.cprint(self.requestline, 'green')
+
+        # IN this simple server version:
+        # We are NOT processing the client's request
+        # It is a happy server: It always returns a message saying
+        # that everything is ok
+
+        # Message to send back to the clinet
+        contents = "I am the happy server! :-)"
+
+        # Generating the response message
+        self.send_response(200)  # -- Status line: OK! #1El navegador ba a ser contestado.
+
+        # Define the content-type header:
+        self.send_header('Content-Type', 'text/plain') #2Código plano sin formato, no html.
+        self.send_header('Content-Length', len(str.encode(contents))) #Conocer numero de bytes codificados.
+
+        # The header is finished
+        self.end_headers() #3
+
+        # Send the response message
+        self.wfile.write(str.encode(contents)) #4Enviar el contenido.
+
+        return
+
+
+# ------------------------
+# - Server MAIN program
+# ------------------------
+# -- Set the new handler
+Handler = TestHandler
+
+# -- Open the socket server
+with socketserver.TCPServer(("", PORT), Handler) as httpd:
+
+    print("Serving at PORT", PORT)
+
+    # -- Main loop: Attend the client. Whenever there is a new
+    # -- clint, the handler is called
+    try:
+        httpd.serve_forever()
+    except KeyboardInterrupt:
+        print("")
+        print("Stoped by the user")
+        httpd.server_close()
+
+print("")
+print("Server Stopped")
