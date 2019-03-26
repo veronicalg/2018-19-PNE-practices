35fa8ceb00d7b8b1c19b3feb67116caab10fa29e
veronicalg <veronicalopezgonzal@hotmail.com>
2019-03-26 18:54:53 +0100

    Errors have been fixed in case woeid is not know. Exercise 2 from session 19 finished (weather program).

--- session-19/exercise2.py
+++ session-19/exercise2.py
@@ -27,7 +27,7 @@ conn = http.client.HTTPSConnection(HOSTNAME)
 
 # -- Send the request. No body (None)
 # -- Use the defined headers
-conn.request(METHOD, ENDPOINT + LOCATION.upper(), None, headers)
+conn.request(METHOD, ENDPOINT + LOCATION.lower(), None, headers)
 
 # -- Wait for the server's response
 r1 = conn.getresponse()
@@ -49,51 +49,54 @@ conn.close()
 
 # -- Generate the object from the json file in a list
 city_info_list = json.loads(raw_info)
+#print(city_info_list)
 
+
+# -- Fixing errors in case the city is not in the database
+if len(city_info_list)==0:
+    print("Sorry, this city is not in our database.")
+else:
 # -- Getting WOEID identifier number
-WOEID = city_info_list[0]['woeid']
+    WOEID = city_info_list[0]['woeid']
 
 
+    #--Establishing a new connection in order to retrieve the data
+
+    ENDPOINT= "/api/location/"
+    conn = http.client.HTTPSConnection(HOSTNAME)
+
+    # -- Send the request with the WOEID now.
+    # -- Use the defined headers
+    conn.request(METHOD, ENDPOINT + str(WOEID) + '/', None, headers)
+
+    # -- Wait for the server's response
+    r1 = conn.getresponse()
+
+    # -- Print the status
+    print()
+    print("Response received: ", end='')
+    print(r1.status, r1.reason)
+
+    text_raw = r1.read().decode("utf-8")
+    conn.close()
+
+    # -- Optionally you can print the
+    # -- received json file for testing
+    #print(text_raw)
+
+    # -- Generate the object from the json file
+    weather = json.loads(text_raw)
 
 
+    # -- Get the data
+    place = weather['title']
+    time = weather['time']
+    description= weather['consolidated_weather'][0]['weather_state_name']
+    temp = weather['consolidated_weather'][0]['the_temp']
 
+    print()
+    print("Place: {}".format(place))
+    print("Time: {}".format(time))
+    print("Weather description: {}".format(description))
+    print("Current temperature: {} degrees".format(temp))
 
-#--Establishing a new connection in order to retrieve the data
-
-ENDPOINT= "/api/location/"
-conn = http.client.HTTPSConnection(HOSTNAME)
-
-# -- Send the request with the WOEID now.
-# -- Use the defined headers
-conn.request(METHOD, ENDPOINT + str(WOEID) + '/', None, headers)
-
-# -- Wait for the server's response
-r1 = conn.getresponse()
-
-# -- Print the status
-print()
-print("Response received: ", end='')
-print(r1.status, r1.reason)
-
-text_raw = r1.read().decode("utf-8")
-conn.close()
-
-# -- Optionally you can print the
-# -- received json file for testing
-#print(text_raw)
-
-# -- Generate the object from the json file
-weather = json.loads(text_raw)
-
-
-# -- Get the data
-place = weather['title']
-time = weather['time']
-description= weather['consolidated_weather'][0]['weather_state_name']
-temp = weather['consolidated_weather'][0]['the_temp']
-
-print()
-print("Place: {}".format(place))
-print("Time: {}".format(time))
-print("Weather description: {}".format(description))
-print("Current temperature: {} degrees".format(temp))
